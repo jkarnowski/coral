118c17dede31d39dd145a2c24b0a1c4aa57b99d8
Alex White <awhit012@gmail.com>
2015-08-15 14:50:04 -0700

    adds criterion controller, model, migration, runs migration

---
+++ app/helpers/criterion_helper.rb
@@ -0,0 +1,2 @@
+module CriterionHelper
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815214128) do
+ActiveRecord::Schema.define(version: 20150815214548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "criterions", force: true do |t|
+    t.integer  "rubric_id"
+    t.integer  "rating"
+    t.boolean  "enabled"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "feedbacks", force: true do |t|
     t.integer  "video_id"
