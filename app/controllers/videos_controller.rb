8bc40a9328e33cad00890627d6e99589974788a5
daschi <daschi87@gmail.com>
2015-08-15 19:24:26 -0700

    Update youtube embed URL create action so it accepts URLs from playlists too.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,8 +8,11 @@ class VideosController < ApplicationController
   def new
   	video_params = params[:video]
   	@video = Video.create(embedded_url: video_params[:embedded_url])
-  	@youtube_id = @video.embedded_url[/(=[\d\D]{5,})/].slice(1..-1)
-		p "*" * 100
-  	p @youtube_id
+  	@youtube_id = @video.embedded_url[/(=[\d\D]{5,})&/].slice(1..-1)
+  	if @youtube_id[-1] == "&"
+  		@youtube_id.slice!(-1)
+  	end
+  	@youtube_id
   end
+
 end
