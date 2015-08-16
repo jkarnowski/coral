597a27b5e9ccaf72fb9aea181b6afb2573aba91b
daschi <daschi87@gmail.com>
2015-08-15 18:57:02 -0700

    create videos on videos/new and display in embedded youtube iframe.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,4 +4,12 @@ class VideosController < ApplicationController
 
   def show
   end
+
+  def new
+  	video_params = params[:video]
+  	@video = Video.create(embedded_url: video_params[:embedded_url])
+  	@youtube_id = @video.embedded_url[/(=[\d\D]{5,})/].slice(1..-1)
+		p "*" * 100
+  	p @youtube_id
+  end
 end
