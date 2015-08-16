633ccca042b54d35928b9a5e456852f8367fe1a1
daschi <daschi87@gmail.com>
2015-08-15 21:45:56 -0700

    Remove regex for playlist videos. Can only upload videos not from a playlist for youtube.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,21 +8,16 @@ class VideosController < ApplicationController
   end
 
   def new
-  	video_params = params[:video]
+    video_params = params[:video]
+
   	@video = Video.create(
   		teacher_id: session[:id], 
   		embedded_url: video_params[:embedded_url]
   		)
   	# Working on using a module here from videos_helper
   	# @youtube_id = get_embed_code(@video)
-  	# p @youtube_id
-  	# p video_params
-  	# p @video
-  	@youtube_id = @video.embedded_url[/(=[\d\D]{5,}&{1})/].slice(1..-1)
-  	if @youtube_id[-1] == "&"
-  		@youtube_id.slice!(-1)
-  	end
-  	@youtube_id
+
+    @youtube_id = @video.embedded_url[/(=[\d\D]{5,})/].slice(1..-1)
   end
 
 end
