aad50c740b82642f5c434d0fdf83ccdbf99072cb
Alex White & Jaclyn <pair+awhit012.jkarnowski@devbootcamp.com>
2015-08-15 20:40:34 -0700

    creates form to add content to video feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -11,4 +11,10 @@ class FeedbacksController < ApplicationController
     Feedback.create(feedback_params)
     # redirect_to
   end
+
+    private
+
+  def feedback_params
+    params.require(:feedbacks).permit(:content)
+  end
 end
