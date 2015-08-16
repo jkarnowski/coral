73263fb3e52d903b919c079d7ef8d70645df5eee
Alex White <awhit012@gmail.com>
2015-08-16 12:54:47 -0700

    adds youtube_id to video table, uses this id to render videos on show and index pages

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -10,21 +10,24 @@ class VideosController < ApplicationController
 
   def show
     @video = Video.find_by(id: params[:id])
-    @youtube_id = @video.embedded_url[/(=[\d\D]{5,})/].slice(1..-1)
+    url = @video.embedded_url || ""
+    # temp = url[/(=[\d\D]{5,})/] || ""
+    # @youtube_id = temp.slice(1..-1)
     @rubric = Rubric.new
   end
 
   def new
-    video_params = params[:video]
-
+    video_params = params[:video] || params[:videos] || {}
+    temp = @video.embedded_url[/(=[\d\D]{5,})/] || ""
+    youtube_id = temp.slice(1..-1)
   	@video = Video.create(
   		teacher_id: session[:id],
-  		embedded_url: video_params[:embedded_url]
+  		embedded_url: video_params[:embedded_url] || "",
+      title: video_params[:title],
+      youtube_id: youtube_id
   		)
   	# Working on using a module here from videos_helper
   	# @youtube_id = get_embed_code(@video)
-
-    @youtube_id = @video.embedded_url[/(=[\d\D]{5,})/].slice(1..-1)
   end
 
 end
