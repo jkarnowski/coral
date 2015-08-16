8568b2ffa453ef88b05f6fa60c17f56c320250a8
daschi <daschi87@gmail.com>
2015-08-15 20:18:34 -0700

    Add videos_helper method to get embed code. Currently not working needs to be debugged

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,5 @@
 class VideosController < ApplicationController
+	include VideosHelper
   def index
   end
 
@@ -7,8 +8,16 @@ class VideosController < ApplicationController
 
   def new
   	video_params = params[:video]
-  	@video = Video.create(embedded_url: video_params[:embedded_url])
-  	@youtube_id = @video.embedded_url[/(=[\d\D]{5,})&/].slice(1..-1)
+  	@video = Video.create(
+  		teacher_id: session[:id], 
+  		embedded_url: video_params[:embedded_url]
+  		)
+  	# Working on using a module here from videos_helper
+  	# @youtube_id = get_embed_code(@video)
+  	# p @youtube_id
+  	# p video_params
+  	# p @video
+  	@youtube_id = @video.embedded_url[/(=[\d\D]{5,}&{1})/].slice(1..-1)
   	if @youtube_id[-1] == "&"
   		@youtube_id.slice!(-1)
   	end
